8f9d7bc576f5b80e37b43e2c2d71fbed4a05ea1f
YaoZengzeng <yaozengzeng@zju.edu.cn>
2019-04-25 09:24:52 +0800

    organize code

--- prometheus/controller/controller.go
+++ prometheus/controller/controller.go
@@ -38,7 +38,7 @@ type queryData struct {
 	Result     model.Value `json:"result"`
 }
 
-type Response struct {
+type queryResult struct {
 	Status status      `json:"status"`
 	Data   interface{} `json:"data,omitempty"`
 	Error  string      `json:"error,omitempty"`
@@ -83,7 +83,7 @@ func parseDuration(s string) (time.Duration, error) {
 	return 0, fmt.Errorf("cannot parse %q to a valid duration", s)
 }
 
-func (p *PrometheusController) MonitorPod() {
+func (p *PrometheusController) QueryPod() *queryResult {
 	cluster := p.GetString(":cluster")
 	namespace := p.GetString(":namespace")
 	pod := p.GetString(":pod")
@@ -91,32 +91,50 @@ func (p *PrometheusController) MonitorPod() {
 
 	client, err := p.getClient(&DataSource{Url: "http://localhost:9090"})
 	if err != nil {
-		logs.Error("Get Prometheus client failed: %v", err)
+		return &queryResult{
+			Status:	statusError,
+			Error:	fmt.Sprintf("Get Prometheus client failed: %v", err),
+		}
 	}
 
 	r := p.Ctx.Request
 
 	start, err := parseTime(r.FormValue("start"))
 	if err != nil {
-		logs.Error("Parse start time failed: %v", err)
+		return &queryResult{
+			Status:	statusError,
+			Error:	fmt.Sprintf("Parse start time failed: %v", err),
+		}
 	}
 
 	end, err := parseTime(r.FormValue("end"))
 	if err != nil {
-		logs.Error("Parse end time failed: %v", err)
+		return &queryResult{
+			Status:	statusError,
+			Error:	fmt.Sprintf("Parse end time failed: %v", err),
+		}
 	}
 
 	if end.Before(start) {
-		logs.Error("End before start")
+		return &queryResult{
+			Status:	statusError,
+			Error:	fmt.Sprintf("End before start"),
+		}
 	}
 
 	step, err := parseDuration(r.FormValue("step"))
 	if err != nil {
-		logs.Error("Parse step failed: %v", err)
+		return &queryResult{
+			Status:	statusError,
+			Error:	fmt.Sprintf("Parse step failed: %v", err),
+		}
 	}
 
 	if step <= 0 {
-		logs.Error("Zero or negative query resolution step width are not accepted")
+		return &queryResult{
+			Status:	statusError,
+			Error:	fmt.Sprintf("Zero or negative query resolution step width are not accepted"),
+		}
 	}
 
 	timeRange := apiv1.Range{
@@ -131,35 +149,55 @@ func (p *PrometheusController) MonitorPod() {
 	for _, query := range queries {
 		value, err := client.QueryRange(context.Background(), query, timeRange)
 		if err != nil {
-			logs.Error("Query Prometheus failed: %v", err)
+			return &queryResult{
+				Status:	statusError,
+				Error:	fmt.Sprintf("Query Prometheus failed: %v", err),
+			}
 		}
 		matrix, ok := value.(model.Matrix)
 		if !ok {
-			logs.Error("The type of QueryRange value is unexpected")
+			return &queryResult{
+				Status:	statusError,
+				Error:	fmt.Sprintf("The type of QueryRange value is unexpected"),
+			}
 		}
 		// Actually the maxtrix length should always be 1.
 		if len(matrix) == 0 {
-			logs.Error("The length of QueryRange value is 0")
+			return &queryResult{
+				Status:	statusError,
+				Error:	fmt.Sprintf("The length of QueryRange value is 0"),
+			}
 		}
 		// Add name label to let frontend know the meaning of corresponding samples.
 		matrix[0].Metric["name"] = "cpu_usage"
 		data = append(data, matrix[0])
 	}
 
-	b, err := json.Marshal(&Response{
-		Status: statusSuccess,
-		Data: &queryData{
-			ResultType: "matrix",
-			Result:     data,
+	return &queryResult{
+		Status:	statusSuccess,
+		Data:	&queryData{
+			ResultType:	"matrix",
+			Result:		data,
 		},
-	})
+	}
+}
+
+func (p *PrometheusController) MonitorPod() {
+	w := p.Ctx.ResponseWriter
+	result := p.QueryPod()
+	b, err := json.Marshal(result)
 	if err != nil {
-		logs.Error("Marshal response failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	w := p.Ctx.ResponseWriter
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	if result.Status == statusSuccess {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		// More refined in the future.
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 
 	if n, err := w.Write(b); err != nil {
 		logs.Error("Write response body failed: %v, bytesWritten: %v", err, n)
